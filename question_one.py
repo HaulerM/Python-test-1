8811c1c01609011a506b06469b8e23d4dbecbc74
HaulerM <haulermusa18@gmail.com>
2024-10-10 00:48:34 -0700

    test 1

---
+++ question_one.py
@@ -0,0 +1,6 @@
+#Question 1(a)
+#Create the python program that caculates the area of a triangle
+base = float(input("Enter the base of a traiangle: "))
+height =float(input("Enter the height of a triangle: "))
+area_of_a_triangle =(1/2 * base * height)
+print(f"The area of a triangle is {area_of_a_triangle}")
